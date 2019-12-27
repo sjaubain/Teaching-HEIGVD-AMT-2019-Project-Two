1bff9bd1173a2245ccb07a6b04302497af8d8349
simon <simon.jobin@heig-vd.ch>
2019-12-27 22:20:29 +0100

    end code gen from maven build

---
+++ swagger/spring-server/src/main/java/ch/heigvd/authentication/api/ApiException.java
@@ -0,0 +1,11 @@
+package ch.heigvd.authentication.api;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2019-12-27T22:12:46.243+01:00")
+
+public class ApiException extends Exception{
+	private int code;
+	public ApiException (int code, String msg) {
+		super(msg);
+		this.code = code;
+	}
+}
---
+++ swagger/spring-server/src/main/java/ch/heigvd/authentication/api/NotFoundException.java
@@ -0,0 +1,11 @@
+package ch.heigvd.authentication.api;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2019-12-27T22:12:46.243+01:00")
+
+public class NotFoundException extends ApiException {
+	private int code;
+	public NotFoundException (int code, String msg) {
+		super(code, msg);
+		this.code = code;
+	}
+}
